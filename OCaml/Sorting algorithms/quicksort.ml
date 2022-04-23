33d2417d21f4a7a63a64f6f486a3016e574982f4
HegManga <28701547+HegManga@users.noreply.github.com>
2022-04-23 10:58:21 +0200

    Update quicksort.ml

--- OCaml/Sorting algorithms/quicksort.ml
+++ OCaml/Sorting algorithms/quicksort.ml
@@ -1,12 +1,37 @@
 (*
 NOW ls:int list 
 *)
-let quicksort ls = 
+let quicksort_real ls = 
   let rec pivot lis p res=
     match (lis,res) with
     |(h::t,(l,c,r)) when h > p -> pivot t p (l,c,h::r)
     |(h::t,(l,c,r)) when h = p -> pivot t p (l,h::c,r)                              
     |(h::t,(l,c,r)) when h < p -> pivot t p (h::l,c,r)
+    |([],_)-> res
+  and qsort list =
+    match list with
+    |[]-> list
+    |[x] -> list
+    |h::t -> 
+        let (lp,cp,rp) = pivot t h ([],[],[])
+        in List.append (List.append (qsort (lp)) (h::cp)) (qsort rp)
+  in 
+  qsort ls
+;;
+
+(*
+  f: 'a -> 'a -> 'int
+  f is a partial order relation
+    if a<b then -1 
+       a=b then 0
+       a>b then 1
+*)
+let quicksort f ls = 
+  let rec pivot lis p res=
+    match (lis,res) with
+    |(h::t,(l,c,r)) when (f h p) =  1 -> pivot t p (l,c,h::r)
+    |(h::t,(l,c,r)) when (f h p) =  0 -> pivot t p (l,h::c,r)                              
+    |(h::t,(l,c,r)) when (f h p) = -1 -> pivot t p (h::l,c,r)
     |([],_)-> res
   and qsort list =
     match list with
